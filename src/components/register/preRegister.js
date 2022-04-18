24dc271111a929ce5cb5c087b161a95ea20dece2
Kingsley Daniel <admin@Kingsley.local>
2022-04-18 18:40:49 +0100

    pre registration ready

--- src/components/register/preRegister.js
+++ src/components/register/preRegister.js
@@ -1,23 +1,22 @@
 import { Formik } from 'formik';
 import React from 'react';
 import axios from 'axios';
-// import Textfield from '../utilities/formFields/textfield';
+import Modal from 'react-modal';
+import {Link} from 'react-router-dom';
 import '../styles/components/register/_pre-register.scss';
 
 const PreRegister = () => {
-    const [success, setSuccess] = React.useState(false);
+    const [modal, setModal] = React.useState(false);
 
-    const onSubmit = async () => {
-        const payload = {
-            name: 'Kingsley',
-        }
-
+    const onSubmit = async (values) => {
         try {
-            const response = await axios.post('', payload)
-            if(response.status === 200){
-                setSuccess(true);
-            }
-
+            const response = await axios.post(
+                'https://kiddiescrown.com/api/user/pre-register',
+                // 'http://localhost:4000/api/user/pre-register',
+                values
+            )
+            console.log(response);
+            setModal(true)
         } catch (error) {
             console.log(error);
         }
@@ -26,6 +25,27 @@ const PreRegister = () => {
     return (
         <div className='pre-register'>
             <div>
+                <Modal
+                    isOpen = {modal}
+                    shouldFocusAfterRender = {true}
+                    preventScroll = {true}
+                    contentLabel = 'Registration status'
+                    className = {'ReactModal__Content'}
+                >
+                    <h1> Success! </h1>
+                    <p> You'll be contacted within 24hrs </p>
+                    <Link 
+                        to = '/'> 
+                            <input 
+                                type = 'button' 
+                                value = 'Okay'
+                                onClick = {()=> {
+                                    setModal(false);
+                                }} 
+                                className = 'btn--primary'
+                            /> 
+                    </Link>
+                </Modal>
                 <h3>Registration Form</h3>
                 <Formik
                     initialValues={{
@@ -34,36 +54,40 @@ const PreRegister = () => {
                         phone2: '' 
                     }}
                     onSubmit={(values, { setSubmitting }) => {
-                        console.log(values)
+                        console.log(values);
+                        onSubmit(values);
                     }}
                 >
                     {(formikProps) => (
                         <form onSubmit={formikProps.handleSubmit}>
                             <input
-                            type="text"
-                            name="name"
-                            onChange={formikProps.handleChange}
-                            onBlur={formikProps.handleBlur}
-                            value={formikProps.values.name}
-                            placeholder='Your name'
+                                type="text"
+                                name="name"
+                                onChange={formikProps.handleChange}
+                                onBlur={formikProps.handleBlur}
+                                value={formikProps.values.name}
+                                placeholder='Your name'
+                                required
                             />
                             
                             <input
-                            type="text"
-                            name="phone1"
-                            onChange={formikProps.handleChange}
-                            onBlur={formikProps.handleBlur}
-                            value={formikProps.values.phone1}
-                            placeholder='Phone number'
+                                type="text"
+                                name="phone1"
+                                onChange={formikProps.handleChange}
+                                onBlur={formikProps.handleBlur}
+                                value={formikProps.values.phone1}
+                                placeholder='Phone number'
+                                required
                             />
                             
                             <input
-                            type="text"
-                            name="phone2"
-                            onChange={formikProps.handleChange}
-                            onBlur={formikProps.handleBlur}
-                            value={formikProps.values.phone2}
-                            placeholder='Whatsapp number'
+                                type="text"
+                                name="phone2"
+                                onChange={formikProps.handleChange}
+                                onBlur={formikProps.handleBlur}
+                                value={formikProps.values.phone2}
+                                placeholder='Whatsapp number'
+                                required
                             />
                             
                             <input type='submit' value='SUBMIT' disabled={formikProps.isSubmitting}/>
@@ -76,3 +100,5 @@ const PreRegister = () => {
 }
     
 export default PreRegister;
+
+
--- src/components/register/regModal.js
+++ src/components/register/regModal.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Modal from 'react-modal'
-import {Link} from 'react-router-dom'
-import '../styles/components/register/_regModal.scss'
+import React from 'react';
+import Modal from 'react-modal';
+import {Link} from 'react-router-dom';
+import '../styles/components/register/_regModal.scss';
 
 const RegModal = (props) => {
 
